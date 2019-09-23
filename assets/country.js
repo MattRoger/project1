8d0c4028411675a5f760fe9266a3be5c9b2da9f6
James Snell <Vulpes3.1415@yahoo.com>
2019-09-23 13:52:40 -0600

    Added modal button

--- assets/country.js
+++ assets/country.js
@@ -1,33 +1,41 @@
-
+var countrySearch;
 console.log("start");
-$("#search").on("click",function(event){
+
+// If statement with model linked to empty string
+$("#search").on("click", function (event) {
     event.preventDefault();
     console.log("clicked")
 
-    var countrySearch=$("#cSearch").val().trim()
+    var countrySearch = $("#cSearch").val().trim()
 
 
-    var countryURL= "https://restcountries.eu/rest/v2/name/"+ countrySearch;
-    
-    $.ajax({
-        url:countryURL,
-        method: "GET"
-    }).then (function(response){
-        $("#name").text("Country: " + response[0].nativeName)
-        $("#capital").text("Capital: " +response[0].capital)
-        $("#population").text("Population: " +response[0].population)
-        $("#region").text("Region: " +response[0].region)
-        $("#currency").text("Currency: " +response[0].currencies[0].name)
-        console.log(response)
-        console.log(response[0].nativeName)
-        console.log(response[0].capital)
-        console.log(response[0].population)
-        console.log(response[0].region)
-        console.log(response[0].currencies[0].name)
-    })
+    var countryURL = "https://restcountries.eu/rest/v2/name/" + countrySearch;
 
-    
+    if (countrySearch !== "") {
+        $.ajax({
+            url: countryURL,
+            method: "GET"
+        }).then(function (response) {
+            $("#name").text("Country: " + response[0].nativeName)
+            $("#capital").text("Capital: " + response[0].capital)
+            $("#population").text("Population: " + response[0].population)
+            $("#region").text("Region: " + response[0].region)
+            $("#currency").text("Currency: " + response[0].currencies[0].name)
+            console.log(response)
+            console.log(response[0].nativeName)
+            console.log(response[0].capital)
+            console.log(response[0].population)
+            console.log(response[0].region)
+            console.log(response[0].currencies[0].name)
+        })
 
+
+} else { 
+
+$("#enterCountry").css({display:"block"})
+}
 })
 
-
+$("#close").on("click", function(){
+    $("#enterCountry").css({display:"none"})
+})
